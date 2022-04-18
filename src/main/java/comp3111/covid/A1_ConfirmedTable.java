efd0187c8abe694f5feec2f123632a06b283ca95
hengjuice <hengjiuxiao@gmail.com>
2022-04-18 14:50:49 +0800

    Added UI UX to Tabs A2, B2, C2. Deleted console (and its panel which is at the bottom of the GUI)

---
+++ src/main/java/comp3111/covid/A1_ConfirmedTable.java
@@ -0,0 +1,18 @@
+package comp3111.covid;
+
+/*
+Task A1: Generate data tables showing the number of confirmed COVID-19 cases by country
+
+Input
+•	Specify the date of interest
+•	Specify the countries of interest
+
+Output
+•	Label the data table with a proper title: "Number of Confirmed COVID-19 Cases as of [Date of Interest]"
+•	Present data in 3 columns with proper headings of "Country", "Total Cases", and "Total Cases (per 1M)"
+•	Present data for the countries of interest in rows
+*/
+
+public class A1_ConfirmedTable {
+
+}
--- src/main/java/comp3111/covid/A2_ConfirmedChart.java
+++ src/main/java/comp3111/covid/A2_ConfirmedChart.java
@@ -1,19 +1,18 @@
 package comp3111.covid;
 
-
 /*
-Task C2: Generate data charts showing the cumulative rate of vaccination against COVID-19 by country
+Task A2: Generate data charts showing the cumulative confirmed COVID-19 cases (per 1M) by country
 
 Input
 •	Specify the period of interest
 •	Specify the countries of interest
 
 Output
-•	Label the data chart with a proper title: "Cumulative Rate of Vaccination against COVID-19 "
+•	Label the data chart with a proper title: "Cumulative Confirmed COVID-19 Cases (per 1M)"
 •	Label the x-axis and y-axis with proper scales
 •	Present data in coloured cumulative curves labelled with corresponding countries
 */
 
-public class C2_VaccinationsChart {
-
+public class A2_ConfirmedChart {
+	
 }
