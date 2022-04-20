656545a19355336a649d7b22d9eb41eaaeb86d1f
Forrest-J <zfjiang@ust.hk>
2022-04-20 17:56:09 +0800

    InputChecker Test

---
+++ src/test/java/comp3111/covid/InputCheckerTester.java
@@ -0,0 +1,26 @@
+package comp3111.covid;
+
+import static org.junit.Assert.*;
+
+import java.time.LocalDate;
+
+import org.junit.Test;
+
+public class InputCheckerTester {
+
+	
+	@Test
+	public void startAfterEnd() {
+    	LocalDate startDate = LocalDate.parse("2020-09-05");
+    	
+    	LocalDate endDate = LocalDate.parse("2019-09-05");
+
+    	// CHECK INPUT VALIDITY
+    	
+    	// Initialize InputChecker Object - will return any errors
+    	InputChecker inputChecker = new InputChecker(startDate, endDate);
+    	inputChecker.chartInputValidityCheck();
+		assertEquals(InputChecker.error_statements.get(0), "End Date cannot be before Start Date");
+	}
+
+}
