3859156fe63d738f1b0fb0f514ceb43b478cb9c9
Forrest-J <zfjiang@ust.hk>
2022-04-20 18:05:09 +0800

    No end date test

--- src/test/java/comp3111/covid/InputCheckerTester.java
+++ src/test/java/comp3111/covid/InputCheckerTester.java
@@ -22,5 +22,15 @@ public class InputCheckerTester {
     	inputChecker.chartInputValidityCheck();
 		assertEquals(InputChecker.error_statements.get(0), "End Date cannot be before Start Date");
 	}
+	public void noEnd() {
+    	LocalDate startDate = LocalDate.parse("2020-09-05");
+
+    	// CHECK INPUT VALIDITY
+    	
+    	// Initialize InputChecker Object - will return any errors
+    	InputChecker inputChecker = new InputChecker(startDate);
+    	inputChecker.chartInputValidityCheck();
+		assertEquals(InputChecker.error_statements.get(0), "End Date not provided");
+	}
 
 }
